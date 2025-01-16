445096065d53b577b62af22d7e01bfdf327725f6
“Maite” <maitesolam@gmail.com>
2025-01-16 08:35:05 +0100

    Proyecto final

--- Calculadora.java
+++ Calculadora.java
@@ -9,16 +9,13 @@ public class Calculadora {
 
 		Scanner scan = new Scanner(System.in);
 
-		float numero1, numero2;
+		float num1, num2; 
 		int leer;
-		float resultado;
-		
-
 		System.out.println("CALCULADORA");
 		System.out.println("Introduce el primer número: ");
-		numero1 = Integer.parseInt(scan.nextLine());
+		num1 = Integer.parseInt(scan.nextLine());
 		System.out.println("Introduce el segundo número: ");
-		numero2 = Integer.parseInt(scan.nextLine());
+		num2 = Integer.parseInt(scan.nextLine());
 		do {
 		System.out.println("¿Qué deseas hacer con esos números?");
 		System.out.println("1. Sumar");
@@ -31,20 +28,16 @@ public class Calculadora {
 		switch (leer) {
 
 		case 1:
-			resultado= numero1+ numero2;
-			System.out.println("El resultado es "+resultado);
-			//FIXME FALTA UN BREAK
+			sumar(num1, num2);
+			break;
 		case 2:
-			resultado= numero1-numero2;
-			System.out.println("El resultado es "+resultado);
+			restar(num1, num2);
 			break;
 		case 3:
-			resultado= numero1*numero2;
-			System.out.println("El resultado es "+resultado);
+			multiplicar(num1, num2);
 			break;
 		case 4:
-			resultado=numero1/numero2;
-			System.out.println("El resultado es "+resultado);
+			dividir(num1, num2);
 			break;
 			
 		default:
@@ -56,6 +49,52 @@ public class Calculadora {
 		
 	scan.close();	
 }
+	/**
+	 * Método para dividir dos números de tipo float y devolver un valor de tipo float
+	 * @param num1 Primer número que se dividirá
+	 * @param num2 Segundo número que se dividirá
+	 * @return Resultado de la división de num1 y num2
+	 * */
+
+	private static void dividir(float num1, float num2) {
+		float resultado;
+		resultado=num1/num2;
+		System.out.println("El resultado es "+resultado);
+	}
+	/**
+	 * Método para multiplicar dos números de tipo float y devolver un valor de tipo float
+	 * @param num1 Primer número que se multiplicará
+	 * @param num2 Segundo número que se multiplicará
+	 * @return Resultado de la multiplicación de num1 y num2
+	 * */
+
+	private static void multiplicar(float num1, float num2) {
+		float resultado;
+		resultado= num1*num2;
+		System.out.println("El resultado es "+resultado);
+	}
+/**
+ * Método para restar dos números de tipo float y devolver un valor de tipo float
+ * @param num1 Primer número que se restará
+ * @param num2 Segundo número que se restará
+ * @return Resultado de la resta de num1 y num2
+ */
+	private static void restar(float num1, float num2) {
+		float resultado;
+		resultado= num1-num2;
+		System.out.println("El resultado es "+resultado);
+	}
+	/**
+	 * Método para sumar dos números de tipo float y devolver un valor de tipo float
+	 * @param num1 Primer número que se sumará
+	 * @param num2 Segundo número que se sumará
+	 * @return Resultado de la suma de num1 y num2
+	 */
+	private static void sumar(float num1, float num2) {
+		float resultado;
+		resultado= num1+ num2;
+		System.out.println("El resultado es "+resultado);
+	}
 	
 
 
